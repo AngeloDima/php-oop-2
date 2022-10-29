6b992c4d82f7252c394cc1f0712d878ead759650
Angelo <Angelodimauro91@gmail.com>
2022-10-30 01:27:06 +0200

    categoria "cucce"

---
+++ models/cucce.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . `/prodotti.php`;
+
+class cucce extends prodotti{
+
+    public $categoria = "cucce";
+    public $materiale = [];
+    public $peso = 0;
+}
+
+
+
+
+
+
+?>
