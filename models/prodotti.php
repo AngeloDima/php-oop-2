74b34f55c0e8d9b0b2ee9be8bcc9012048e04cf2
Angelo <Angelodimauro91@gmail.com>
2022-10-30 01:16:54 +0200

    construct

--- models/prodotti.php
+++ models/prodotti.php
@@ -5,10 +5,14 @@ class prodotti {
     public $nome;
     public $prezzo = 0;
     public $marca;
-    public $info = ""
+    public $info = "";
 
 
-
+    __construct($_nome, $_marca, $_prezzo){
+        $this->nome = $_nome;
+        $this->marca = $_marca;
+        $this->prezzo = $_prezzo;
+    }
 
     
 }
