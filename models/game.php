325bc04a856935c3daadeecfbe3ee754d178af17
Angelo <Angelodimauro91@gmail.com>
2022-10-30 01:24:48 +0200

    categoria "game"

---
+++ models/game.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . `/prodotti.php`;
+
+class game extends prodotti{
+
+    public $categoria = "giochi";
+    public $materiale = [];
+    public $peso = 0;
+}
+
+
+
+
+
+
+?>
