3aaa74013eab7330931d0d412c49c042a7e5a8e6
Angelo <Angelodimauro91@gmail.com>
2022-10-30 01:23:34 +0200

    categoria "food" + info

--- models/food.php
+++ models/food.php
@@ -5,7 +5,9 @@ require_once __DIR__ . `/prodotti.php`;
 class food extends prodotti{
 
     public $categoria = "food";
-    
+    public $dataDiScadenza = "";
+    public $ingredienti = [];
+    public $peso = 0;
 }
 
 
